d3789586b8160dd5a806540b58365e421bbc1c4f
Matheus Barbosa <matheus.bsa@hotmail.com>
2022-04-30 18:00:36 -0300

    Create aes generator

---
+++ bytes.py
@@ -0,0 +1,23 @@
+def str_to_bytes(s):
+    res = 0
+    for c in s:
+        res <<= 8
+        res |= ord(c)
+
+    return res
+
+def bytes_to_arr(n):
+    res = []
+    while n > 0:
+        res.insert(0, n & 0xff)
+        n >>= 8
+
+    return res
+
+def arr_to_bytes(a):
+    res = 0
+    for c in a:
+        res <<= 8
+        res |= c
+
+    return res
---
+++ util.py
@@ -0,0 +1,54 @@
+def mat_mul(a, b):
+    result = [[0],[0],[0],[0]]
+    for i in range(4):
+        for j in range(4):
+            result[i][0] ^= mul(a[i][j], b[j][0])
+
+    return result
+
+def mul(a, b):
+    res = 0
+    for x in range(b.bit_length() + 1):
+        if b & (1 << x):
+            res ^= a << x
+
+    while res.bit_length() > 8:
+        res ^= 0x11b << (res.bit_length() - 9)
+
+    return res
+
+def rcon(n):
+    res = 1
+    for i in range(n):
+        res = (res<<1) ^ (0x11b & -(res>>7))
+    
+    return res
+
+def processMessage(msg):
+    blocks = []
+    for i in range(len(msg)):
+        if i%16 == 0:
+            blocks.append([])
+        
+        blocks[-1].append(msg[i])
+    
+    while len(blocks[-1]) != 16:
+        blocks[-1].append(ord('{'))
+
+    return blocks
+
+def convert_matrix(block):
+    res = [[] for _ in range(4)]
+    for i in range(len(res)):
+        for j in range(4):
+            res[i].append(block[i+(4*j)])
+
+    return res
+
+def convert_list(block):
+    res = []
+    for i in range(4):
+        for j in range(4):
+            res.append(block[j][i])
+
+    return res
